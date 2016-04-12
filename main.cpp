502b5957f353e4f452d9f08ae24f48bba8c7bb62
Farley <farvelardeg@gmail.com>
2016-04-12 12:49:46 -0500

    ejemplo de multithread

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//ejemplo de multithread
+#include <iostream>
+#include <thread>
+#include <algorithm>
+
+int main()
+{
+    std::vector<std::thread>workers;
+    for(int i=0; i < 10; ++i)
+    {
+        workers.push_back(std::thread([i]()
+        {
+            std::cout<<"Hi from thread "<< i << "!\n";
+        }));
+    }
+    std::cout<<"Hi from main!\n";
+    std::for_each(workers.begin(),workers.end(),[](std::thread & th)
+    {
+        th.join();
+    });
+    return 0;
+}
